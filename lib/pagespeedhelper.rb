c3256ac4458eef6ef15cf2a487698f37d4e59fea
Kirk Stennett <kjsstennett@gmail.com>
2016-08-01 11:48:06 -0700

    refactoring, and made class methods into private methods, updated specs to match

--- lib/pagespeedhelper.rb
+++ lib/pagespeedhelper.rb
@@ -17,27 +17,11 @@ class PageSpeedHelper
     end
   end
 
-  def self.add_protocol_if_absent!(url, secure=false)  
-    if !url.include? "http://" and !url.include? "https://"
-      secure ? url.replace("https://" + url) : url.replace("http://" + url)
-    end
-  end
-
-  def self.build_summary_string!(summary)
-    if summary.to_h.key?(:args=) or summary.to_h.key?(:args)
-      summary.args.each do |arg|
-        summary.format.sub!('{{' + arg.key + '}}', arg.value) if arg.key != 'LINK'
-      end
-    end 
-
-    summary.format.include?(" Learn more") ? summary.format.split(" Learn more")[0] : summary.format 
-  end
-
   def query(urls, secure=false, strategy="desktop")
     @data = []
     @errors = []
     urls = [urls] if !urls.is_a?(Array)
-    @urls = urls.each { |url| PageSpeedHelper.add_protocol_if_absent!(url, secure) }
+    @urls = urls.each { |url| add_protocol_if_absent!(url, secure) }
     @urls.each_slice(20).to_a.each do |url_list|
       send_request(url_list, strategy)
       sleep(0.5)
@@ -46,26 +30,14 @@ class PageSpeedHelper
 
   def parse
     @results = []
-    rule_result_names = ["AvoidLandingPageRedirects", "EnableGzipCompression", "LeverageBrowserCaching",
-                         "MainResourceServerResponseTime", "MinifyCss", "MinifyHTML",
-                         "MinifyJavaScript", "MinimizeRenderBlockingResources", "OptimizeImages",
-                         "PrioritizeVisibleContent"]
    
     @data.each_with_index do |result, i|
       result_hash = Hash.new
       result_hash["url"] = @urls[i]
       result_hash["score"] = result.rule_groups["SPEED"].score
       result_hash["results"] = Hash.new
-      
-      rule_result_names.each do |rule|
-        result_hash["results"][rule] = Hash.new
-        result_hash["results"][rule]["name"] = result.formatted_results.rule_results[rule].localized_rule_name
-        result_hash["results"][rule]["impact"] = result.formatted_results.rule_results[rule].rule_impact
-        
-        if !result.formatted_results.rule_results[rule].summary.nil?
-          result_hash["results"][rule]["summary"] = PageSpeedHelper.build_summary_string!(result.formatted_results.rule_results[rule].summary)
-        end
-      end
+      build_rule_hash(result_hash["results"], result.formatted_results.rule_results)
+
       @results.push(result_hash)
     end
   end
@@ -81,6 +53,39 @@ class PageSpeedHelper
           err.nil? ? @data.push(result) : @errors.push({ "url" => url, "error" => err })
         end
       end
+    end
+  end
+
+  def build_rule_hash(hash, rule_res)
+    rule_names = ["AvoidLandingPageRedirects", "EnableGzipCompression", "LeverageBrowserCaching",
+                  "MainResourceServerResponseTime", "MinifyCss", "MinifyHTML",
+                  "MinifyJavaScript", "MinimizeRenderBlockingResources", "OptimizeImages",
+                  "PrioritizeVisibleContent"]
+  
+    rule_names.each do |rule|
+      hash[rule] = Hash.new
+      hash[rule]["name"] = rule_res[rule].localized_rule_name
+      hash[rule]["impact"] = rule_res[rule].rule_impact
+        
+      if !rule_res[rule].summary.nil?
+        hash[rule]["summary"] = build_summary_string!(rule_res[rule].summary)
+      end
+    end
+  end
+  
+  def build_summary_string!(summary)
+    if summary.to_h.key?(:args)
+      summary.args.each do |arg|
+        summary.format.sub!('{{' + arg.key + '}}', arg.value) if arg.key != 'LINK'
+      end
+    end 
+
+    summary.format.include?(" Learn more") ? summary.format.split(" Learn more")[0] : summary.format 
+  end
+
+  def add_protocol_if_absent!(url, secure=false)  
+    if !url.include? "http://" and !url.include? "https://"
+      secure ? url.replace("https://" + url) : url.replace("http://" + url)
     end
   end
 
